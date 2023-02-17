6b4e4640d49ad6afdcba858ae1c39577767722af
nguyenisabel <122078261+nguyenisabel@users.noreply.github.com>
2023-02-16 23:09:21 -0800

    add iss location and finish styling

--- getData.js
+++ getData.js
@@ -4,6 +4,18 @@ $.getJSON('http://api.open-notify.org/astros.json?callback=?', function(data) {
     $('#SpacePeople').html(number);
 
     data['people'].forEach(function (d) {
-        $('#astroNames').append('<li>' + d['name']+ '</li>');
+        $('#astroNames').append('<li>' + d['name'] + '   <i>(' + d['craft'] + ')</i>' + '</li>');
     });
 });
+
+function getISS () {
+    $.getJSON('http://api.open-notify.org/iss-now.json?callback=?', function(data) {
+        var lat = data['iss_position']['latitude'];
+        var lon = data['iss_position']['longitude'];
+        $('#isslat').html(Math.round(lat*1000)/1000.0);
+        $('#isslon').html(Math.round(lon*1000)/1000.0);
+    });
+    setTimeout(getISS, 3000); 
+}
+
+getISS();
